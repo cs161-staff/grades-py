cf0d8304e7c3343996332bdfcc2d6d27b0a84efe
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2020-12-03 08:34:35 -0800

    Use dataclasses

--- student.py
+++ student.py
@@ -1,40 +1,35 @@
 import datetime
 import enum
 import statistics
+from dataclasses import dataclass, field
 
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, NamedTuple, Optional, Tuple
 
+@dataclass(frozen=True)
 class Multiplier:
-    def __init__(self, multiplier: float, description: str) -> None:
-        assert multiplier >= 0.0, 'Multiplier cannot be negative'
-        self.multiplier = multiplier
-        self.description = description
+    multiplier: float
+    description: str
 
-    def __repr__(self) -> str:
-        return f'Multiplier({self.multiplier}, {self.description})'
-
+@dataclass
 class Category:
-    def __init__(self, name: str, weight: float, drops: int, slip_days: int, has_late_multiplier: bool = False, override: Optional[float] = None, comments: List[str] = None) -> None:
-        self.name = name
-        self.weight = weight
-        self.drops = drops
-        self.slip_days = slip_days
-        self.has_late_multiplier = has_late_multiplier
-        self.override = override
-        self.comments = comments if comments is not None else []
+    name: str
+    weight: float
+    drops: int
+    slip_days: int
+    has_late_multiplier: bool
+    override: Optional[float] = None
+    comments: List[str] = field(default_factory=list)
 
-    def __repr__(self) -> str:
-        return f'Category({self.name}, {self.weight}, {self.drops}, {self.slip_days}, {self.has_late_multiplier}, {self.override}, {self.comments})'
-
+@dataclass
 class Assignment:
+    @dataclass
     class Grade:
-        def __init__(self, score: float, lateness: datetime.timedelta, multipliers_applied: List[Multiplier] = None, dropped: bool = False, override: Optional[float] = None, comments: List[str] = None) -> None:
-            self.score = score
-            self.lateness = lateness
-            self.multipliers_applied = multipliers_applied if multipliers_applied is not None else []
-            self.dropped = dropped
-            self.override = override
-            self.comments = comments if comments is not None else []
+        score: float
+        lateness: datetime.timedelta
+        multipliers_applied: List[Multiplier] = field(default_factory=list)
+        dropped: bool = False
+        override: Optional[float] = None
+        comments: List[str] = field(default_factory=list)
 
         def get_score(self) -> float:
             score = self.score if self.override is None else self.override
@@ -42,16 +37,12 @@ class Assignment:
                 score *= multiplier.multiplier
             return score
 
-        def __repr__(self) -> str:
-            return f'AssignmentGrade({self.score}, {self.lateness}, {self.multipliers_applied}, {self.dropped}, {self.override}, {self.comments})'
-
-    def __init__(self, name: str, category: str, score_possible: float, weight: float, slip_group: int, grade: Optional['Assignment.Grade'] = None) -> None:
-        self.name = name
-        self.category = category
-        self.score_possible = score_possible
-        self.weight = weight
-        self.slip_group = slip_group
-        self._grade = grade
+    name: str
+    category: str
+    score_possible: float
+    weight: float
+    slip_group: int
+    _grade: Optional['Assignment.Grade'] = None
 
     @property
     def grade(self) -> 'Assignment.Grade':
@@ -62,49 +53,34 @@ class Assignment:
     def grade(self, grade: 'Assignment.Grade') -> None:
         self._grade = grade
 
-    def __repr__(self) -> str:
-        return f'Assignment({self.name}, {self.category}, {self.score_possible}, {self.weight}, {self.slip_group}, {self._grade})'
-
+@dataclass
 class GradeReport:
+    @dataclass
     class CategoryEntry:
-        def __init__(self, raw: float, adjusted: float, weighted: float, comments: List[str]) -> None:
-            self.raw = raw
-            self.adjusted = adjusted
-            self.weighted = weighted
-            self.comments = comments
+        raw: float
+        adjusted: float
+        weighted: float
+        comments: List[str] = field(default_factory=list)
 
-        def __repr__(self) -> str:
-            return f'GradeReport.CategoryEntry({self.raw}, {self.adjusted}, {self.weighted}, {self.comments})'
-
+    @dataclass
     class AssignmentEntry:
-        def __init__(self, raw: float, adjusted: float, weighted: float, comments: List[str]) -> None:
-            self.raw = raw
-            self.adjusted = adjusted
-            self.weighted = weighted
-            self.comments = comments
+        raw: float
+        adjusted: float
+        weighted: float
+        comments: List[str] = field(default_factory=list)
 
-        def __repr__(self) -> str:
-            return f'GradeReport.AssignmentEntry({self.raw}, {self.adjusted}, {self.weighted}, {self.comments})'
+    sid: int
+    student_name: str
+    total_grade: float = 0.0
+    categories: Dict[str, 'GradeReport.CategoryEntry'] = field(default_factory=dict)
+    assignments: Dict[str, 'GradeReport.AssignmentEntry'] = field(default_factory=dict)
 
-    def __init__(self, sid: int, student_name: str, total_grade: float = 0.0, categories: Dict[str, 'GradeReport.CategoryEntry'] = None, assignments: Dict[str, 'GradeReport.AssignmentEntry'] = None) -> None:
-        self.sid = sid
-        self.student_name = student_name
-        self.total_grade = total_grade
-        self.categories = categories if categories is not None else {}
-        self.assignments = assignments if assignments is not None else {}
-
-    def __repr__(self) -> str:
-        return f'GradeReport({self.sid}, {self.student_name}, {self.total_grade}, {self.categories}, {self.assignments})'
-
+@dataclass
 class Student:
-    def __init__(self, sid: int, name: str, categories: Dict[str, Category], assignments: Dict[str, Assignment]) -> None:
-        self.sid = sid
-        self.name = name
-        self.categories = categories
-        self.assignments = assignments
-
-    def __repr__(self) -> str:
-        return f'Student({self.sid}, {self.name}, {self.categories}, {self.assignments})'
+    sid: int
+    name: str
+    categories: Dict[str, Category]
+    assignments: Dict[str, Assignment]
 
     def get_grade_report(self) -> GradeReport:
         grade_report = GradeReport(self.sid, self.name)
